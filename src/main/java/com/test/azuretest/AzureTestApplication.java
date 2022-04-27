eb5f6132eeaff008e6fa55c4e2b2dcf5fe3605f6
studymatlink <study.mat.link@gmail.com>
2022-04-27 13:19:58 +0530

    Update AzureTestApplication.java

--- src/main/java/com/test/azuretest/AzureTestApplication.java
+++ src/main/java/com/test/azuretest/AzureTestApplication.java
@@ -3,6 +3,9 @@ package com.test.azuretest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
 public class AzureTestApplication extends SpringBootServletInitializer {
 
