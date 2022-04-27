8c45a1895da44a51a2e7479bf8ade5d965385c6c
studymatlink <study.mat.link@gmail.com>
2022-04-27 13:17:48 +0530

    Update AzureTestApplication.java

--- src/main/java/com/test/azuretest/AzureTestApplication.java
+++ src/main/java/com/test/azuretest/AzureTestApplication.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AzureTestApplication {
+public class AzureTestApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AzureTestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureTestApplication.class, args);
